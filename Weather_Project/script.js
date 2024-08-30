57bcad22c1a847ceea48e09bee5370902a46280e
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2024-08-30 00:44:36 -0700

    new interfaz and new features in the proyect

--- Weather_Project/script.js
+++ Weather_Project/script.js
@@ -1,19 +1,30 @@
-document.querySelector('input[type="button"]').addEventListener('click', function() {
-  const cityName = document.querySelector('input[type="text"]').value;
-  const apiKey = 'b7d62ddab78f6cbd6a2114407ca4f31c';
-  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-  fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
+document.querySelector('input[type="text"]').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      const cityName = document.querySelector('input[type="text"]').value;
+      const apiKey = 'b7d62ddab78f6cbd6a2114407ca4f31c';
+      const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
           const temp = data.main.temp;
           const weatherDescription = data.weather[0].description;
           const icon = data.weather[0].icon;
           const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
-
+  
           document.querySelector('.grade').innerText = `${temp} °C - ${weatherDescription}`;
-          document.querySelector('img').src = iconUrl;
-          document.querySelector('img').alt = weatherDescription;
-      })
-      .catch(error => console.error('Error:', error));
-});
+          //document.querySelector('img').src = iconUrl;
+          //document.querySelector('img').alt = weatherDescription;
+        })
+        .catch(error => console.error('Error:', error));
+    }
+  });
+  
+  document.querySelector('.clear-click').addEventListener('click', clear);
+  
+  function clear() {
+    document.querySelector('input[type="text"]').value='';
+    document.querySelector('.grade').innerText = '';
+    document.querySelector('img').src = '';
+    document.querySelector('img').alt = '';
+  }
