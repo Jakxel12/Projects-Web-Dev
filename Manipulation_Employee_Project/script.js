5464222b9b45879a71fc93b761259e1e77411325
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2024-09-06 14:12:42 -0700

    first code

---
+++ Manipulation_Employee_Project/script.js
@@ -0,0 +1,9 @@
+import employees from employees.js
+function employeVisual () {
+  
+  const employe = document.querySelector('.display-empployee').innerHTML;
+  employe.forEach(element => {
+    employe.innerHTML =
+    ''
+  });
+}
