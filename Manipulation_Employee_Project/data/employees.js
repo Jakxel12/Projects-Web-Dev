8a841cc6b31013d8c17dd4ebffb6b1f9bd842944
Victor Jakxel Islas Carreon <jakxel20@gmail.com>
2024-09-09 11:56:04 -0700

    changes in css and files

--- Manipulation_Employee_Project/data/employees.js
+++ Manipulation_Employee_Project/data/employees.js
@@ -16,5 +16,41 @@ const employees = [
     img: '/resources/img/user.jpg',
     age: 32,
     description: 'Experienced project manager with a knack for organization and team leadership. Skilled in Agile methodologies and known for delivering projects on time and within budget.'
-  }
+  },
+  {
+    name: 'Employee1',
+    img: '/resources/img/user.jpg',
+    age: 24,
+    description: 'Dedicated and results-driven software engineer with 5 years of experience in developing web applications. He has strong expertise in JavaScript, Python, and SQL. '
+  },
+  {
+    name: 'Employee2',
+    img: '/resources/img/user.jpg',
+    age: 29,
+    description: 'Creative graphic designer with 6 years of experience in brand development and visual storytelling. Expert in Adobe Creative Suite, and known for innovative design solutions that captivate audiences and drive engagement.'
+  },
+  {
+    name: 'Employee3',
+    img: '/resources/img/user.jpg',
+    age: 32,
+    description: 'Experienced project manager with a knack for organization and team leadership. Skilled in Agile methodologies and known for delivering projects on time and within budget.'
+  },
+  {
+    name: 'Employee1',
+    img: '/resources/img/user.jpg',
+    age: 24,
+    description: 'Dedicated and results-driven software engineer with 5 years of experience in developing web applications. He has strong expertise in JavaScript, Python, and SQL. '
+  },
+  {
+    name: 'Employee2',
+    img: '/resources/img/user.jpg',
+    age: 29,
+    description: 'Creative graphic designer with 6 years of experience in brand development and visual storytelling. Expert in Adobe Creative Suite, and known for innovative design solutions that captivate audiences and drive engagement.'
+  },
+  {
+    name: 'Employee3',
+    img: '/resources/img/user.jpg',
+    age: 32,
+    description: 'Experienced project manager with a knack for organization and team leadership. Skilled in Agile methodologies and known for delivering projects on time and within budget.'
+  },
 ];
